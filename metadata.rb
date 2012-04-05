ebdd4b23a060eca9e7c522b0bea81949cca7a716
Richard Henning <rhenning@neat.com>
2012-04-05 09:29:38 -0400

    Adding support for Mac OS X Server platform.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email "mathie@woss.name"
 license          "Apache 2.0"
 description      "Install Homebrew and use it as your package provider in Mac OS X"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0.0"
+version          "1.0.1"
 recipe           "homebrew", "Install Homebrew"
-supports         "mac_os_x"
+%w{ mac_os_x mac_os_x_server }.each do |platform|
+  supports platform
+end
